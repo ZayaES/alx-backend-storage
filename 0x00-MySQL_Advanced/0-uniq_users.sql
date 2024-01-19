a7a0fe3a82688570de257722928fd3dacc038218
Zaya <heavenphrince@gmail.com>
2024-01-19 02:56:17 +0000

    0 -3rd

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
--- creates a table user --
+-- creates a table user
 
----creates a table named user ---
+--creates a table named user
 CREATE TABLE IF NOT EXISTS `users` (
 	id INT IDENTITY(1,1) PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-	);
+	)
